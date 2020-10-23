b779ff915fd7d8d333876609441bea2a6e7576fa
Andy Tran <foolmonkey99@gmail.com>
2020-10-22 19:07:15 -0600

    Add scripts and prefabs for health/mana bar

---
+++ Momentum Knight/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb; 
+    public float speed;
+    public float jumpingForce;
+
+        void Start(){
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+            Jump();
+        }  
+
+        void Movement(){
+            float xChange = Input.GetAxisRaw("Horizontal"); 
+            float movementOffset = xChange * speed; 
+            rb.velocity = new Vector2(movementOffset, rb.velocity.y); 
+        }
+
+        void Jump() { 
+            if (Input.GetKeyDown(KeyCode.Space)) { 
+                rb.velocity = new Vector2(rb.velocity.x, jumpingForce); 
+            } 
+        }
+}
---
+++ Momentum Knight/Assets/UI/UIFollowObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowObject : MonoBehaviour
+{
+    // Camera where UI will be positioned
+    private Camera aCamera;
+
+    // Positions to offset the UI element
+    [SerializeField] public Transform objectToFollow;
+    [SerializeField] public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = aCamera.WorldToScreenPoint(objectToFollow.position + offset);
+        
+        if(transform.position != pos){
+            transform.position = pos;
+        }
+    }
+}
