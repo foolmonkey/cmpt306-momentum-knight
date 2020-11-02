15d4ba79b95b5a6459dd1a431963bd50f789bc88
Kevin <kevin.ken.le@gmail.com>
2020-11-01 20:23:14 -0600

    Created Tile Palletes and Example Intractable Map

---
+++ MKnight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 5.0f;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
