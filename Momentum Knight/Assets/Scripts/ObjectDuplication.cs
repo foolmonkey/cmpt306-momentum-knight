a0d0fdb9417f4b37871fc148a671411f973436ab
Andy T <foolmonkey99@gmail.com>
2020-11-14 15:05:46 -0600

    Change object duplication offset values

--- Momentum Knight/Assets/Scripts/ObjectDuplication.cs
+++ Momentum Knight/Assets/Scripts/ObjectDuplication.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class ObjectDuplication : MonoBehaviour
 {
     public Transform objectsToCreate;
+    public Transform parent;
     public int numRows = 1;
     public int numColumns = 1;
-    public float offsetX = -1f;
-    public float offsetY = -0.5f;
+    public float offsetX = 0.5f;
+    public float offsetY = 0.25f;
 
     void Start()
     {
+        parent = objectsToCreate.parent;
+
         for (int i = 0; i < numRows; i++)
         {
             for (int j = 0; j < numColumns; j++)
             {
-                Instantiate(objectsToCreate, new Vector3(j * offsetX - (offsetX * i), j * offsetY, 0), Quaternion.identity);
+                Instantiate(objectsToCreate, new Vector3((j * offsetX) + (i * offsetX), (j * offsetY) - (i * offsetY), 0), Quaternion.identity, parent);
             }
         }
     }
