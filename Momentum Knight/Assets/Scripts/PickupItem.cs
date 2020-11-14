7ee3d8a34a6e38e64f6b7623807e627b21f6dd8c
Andy T <foolmonkey99@gmail.com>
2020-11-13 22:45:11 -0600

    Add items with animations
    Coin, Health, Mana, Crystals

---
+++ Momentum Knight/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    // public Transform target;
+    
+    private void OnTriggerEnter2D(Collider2D collision){
+       if (collision.gameObject.CompareTag("Player")){
+            // code to pickup item goes here
+            //    target = collision.gameObject.attachedRigidbody.Transform;
+            //    Debug.Log(target);
+            Debug.Log("");
+
+
+       }
+   }
+
+   private void RemoveItem(){
+       Destroy(gameObject);
+   }
+}
