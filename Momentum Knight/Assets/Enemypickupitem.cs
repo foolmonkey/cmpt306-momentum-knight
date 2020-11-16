1294711b6eed48c49fe60d73dc7b6536208054e4
Roxas <rks6412923@gmail.com>
2020-11-15 22:06:55 -0600

    gold eater test(runis and frost)

---
+++ Momentum Knight/Assets/Enemypickupitem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemypickupitem : MonoBehaviour
+{
+    public Transform target;
+    public bool isFollow = false;
+    public float followDistance = 0.5f;
+    public float speedToFollow = 0.5f;
+    private bool hasCollided = false;
+
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Goldeater").GetComponent<Transform>();
+        if (gameObject.tag == "Currency")
+        {
+            isFollow = true;
+        }
+    }
+
+    private void RemoveItem()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        float distanceFromTarget = Vector3.Distance(transform.position, target.position);
+
+        if (isFollow)
+        {
+            if (distanceFromTarget < 0.2f)
+            {
+                RemoveItem();
+            }
+            else if (distanceFromTarget < followDistance || hasCollided)
+            {
+                // change follow speed when too far
+                if (distanceFromTarget > 2)
+                {
+                    speedToFollow += speedToFollow / 3;
+                }
+
+                hasCollided = true;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speedToFollow * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Momentum Knight/Assets/Randomitem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Randomitem : MonoBehaviour
+{
+    public static Randomitem Instance;
+    public GameObject[] Prefabs;
+    public Vector2 minPos, maxPos;
+    [HideInInspector]
+    public int Heart = 0;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        Generate();
+    }
+    public void Generate()
+    {
+        GameObject a = Instantiate(Prefabs[0], new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), 0), Quaternion.identity);
+        GameObject b = Instantiate(Prefabs[1], new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), 0), Quaternion.identity);
+        a.transform.SetParent(this.transform); b.transform.SetParent(this.transform);
+        if (Vector3.Distance(a.transform.position, b.transform.position) < 4f)
+        {
+            Destroy(a); Destroy(b);
+            Generate();
+        }
+    }
+}
