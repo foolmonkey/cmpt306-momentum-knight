99db889720fa360f8731a6ef0299a02909de144a
Roxas <rks6412923@gmail.com>
2020-11-17 21:07:47 -0600

    Enemys on endles run map

--- Momentum Knight/Assets/Swan.cs
+++ Momentum Knight/Assets/Swan.cs
@@ -9,24 +9,23 @@ public class Swan : MonoBehaviour
 {
     public float WaitTime;
     public float ReWaitTime;
+    public float WaitTimeEnd;
     public float Speed;
 
     public Vector2 MoveRandom;
     public Transform TheStarPoint;
     public Transform TheEndPoint;
     public Transform TheTargetPoint;
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (WaitTime >= 0)
         {
-            if (WaitTime > 5)
+            if (WaitTime > WaitTimeEnd)
             {
                 MoveRandom = new Vector2(UnityEngine.Random.Range(TheStarPoint.position.x, TheEndPoint.position.x), UnityEngine.Random.Range(TheStarPoint.position.y, TheEndPoint.position.y));
                 WaitTime = ReWaitTime;
