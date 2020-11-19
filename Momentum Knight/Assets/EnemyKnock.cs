ff55098c07f99554283f7f97dc3ead93581dabc7
Roxas <rks6412923@gmail.com>
2020-11-18 20:18:08 -0600

    knock

---
+++ Momentum Knight/Assets/EnemyKnock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyKnock : MonoBehaviour
+{
+    public int HitDamage = 10;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        PlayerManager healthDown = hit.GetComponent<PlayerManager>();
+        if (healthDown != null)
+        {
+            healthDown.healthDown(HitDamage);
+        }
+        Destroy(this.gameObject);
+    }
+}
+
+
+
+
