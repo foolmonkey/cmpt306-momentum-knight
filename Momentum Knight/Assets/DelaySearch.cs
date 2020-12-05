f2bbacd69bdf6432c11290833b1e87e37eac505f
Roxas <rks6412923@gmail.com>
2020-12-05 16:17:08 -0600

    special AI
    Punish the player's return behavior

---
+++ Momentum Knight/Assets/DelaySearch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+
+public class DelaySearch : MonoBehaviour
+
+{
+    public static IEnumerator DelayToInvokeDo(Action action, float delaySeconds)
+
+    {
+
+        yield return new WaitForSeconds(delaySeconds);
+
+        action();
+
+    }
+
+}
---
+++ Momentum Knight/Assets/Sprites/PunishAI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+enum PunishState
+{
+    Idle,
+    Tracking,
+    Holding,
+    Attack
+}
+public class PunishAI : MonoBehaviour
+{
+    public bool playerIsEnter;
+    public float speed;
+    private Transform playerTran;
+    private PunishState enemyState;
+    private AIPath AIPath;
+    private AIDestinationSetter AIDestinationSetter;
+
+    void Start()
+    {
+        AIPath = GetComponent<AIPath>();
+        AIDestinationSetter = GetComponent<AIDestinationSetter>();
+        playerTran = GameObject.FindGameObjectWithTag("Player").transform;
+        enemyState = PunishState.Idle;
+        AIPath.maxSpeed = 0;
+    }
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case PunishState.Idle:
+                Idle();
+                break;
+            case PunishState.Tracking:
+                Tracking();
+                break;
+            case PunishState.Holding:
+                Holding();
+                break;
+            case PunishState.Attack:
+                Attack();
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void Idle()
+    {
+        if (playerIsEnter && Time.time > 15.0f)
+        {
+            AIPath.maxSpeed = speed;
+            enemyState = PunishState.Tracking;
+        }
+    }
+    public void Tracking()
+    {
+        if (Vector3.Distance(transform.position, playerTran.position) < 12.0f && Vector3.Distance(transform.position, playerTran.position) > 6.0f)
+        {
+            AIPath.maxSpeed = 0;
+            enemyState = PunishState.Holding;
+        }
+        else if (Vector3.Distance(transform.position, playerTran.position) < 6.0f)
+        {
+            AIPath.maxSpeed = 20;
+            enemyState = PunishState.Attack;
+        }
+    }
+    public void Holding()
+    {
+        if (Vector3.Distance(transform.position, playerTran.position) > 12.0f)
+        {
+            AIPath.maxSpeed = speed;
+            enemyState = PunishState.Tracking;
+        }
+        else if (Vector3.Distance(transform.position, playerTran.position) < 6.0f)
+        {
+            AIPath.maxSpeed = 20;
+            enemyState = PunishState.Attack;
+        }
+
+    }
+    public void Attack()
+    {
+        {
+            AIPath.maxSpeed = speed;
+        }
+    }
+
+}
