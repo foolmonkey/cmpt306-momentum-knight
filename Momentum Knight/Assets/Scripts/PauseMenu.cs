d9954e0b7e11951f02f17e70f38ba801f688fd08
Andy Tran <foolmonkey99@gmail.com>
2020-12-07 16:37:15 -0600

    Add pause menu

---
+++ Momentum Knight/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Awake()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
